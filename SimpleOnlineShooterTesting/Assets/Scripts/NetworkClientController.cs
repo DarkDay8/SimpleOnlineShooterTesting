e8abaab0d56b168ddf232ab97a03b2a7a22a4875
Andrey Godlevskiy <darkday8@bigmir.net>
2019-10-20 22:27:37 +0300

    [+] base Client/Server connecting

---
+++ SimpleOnlineShooterTesting/Assets/Scripts/NetworkClientController.cs
@@ -0,0 +1,34 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkClientController : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("don't join random room " + message);
+    }
+}
---
+++ SimpleOnlineShooterTesting/Assets/Scripts/NetworkServerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class NetworkServerController : MonoBehaviourPunCallbacks
+{
+    private Dictionary<int, GameObject> playerListEntries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("We are now connected to the " + PhotonNetwork.CloudRegion + " server!");
+        RoomOptions options = new RoomOptions { MaxPlayers = 3 };
+
+        PhotonNetwork.CreateRoom("TestRoom", options, null);
+    }
+
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Room was been created");
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined to the room");
+        if (playerListEntries == null)
+        {
+            playerListEntries = new Dictionary<int, GameObject>();
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log("Player entered in the room:" + newPlayer.ActorNumber);
+    }
+
+}
